6417f39beb661bb1656a2eddbec11706de587a65
Ashish <khadka99ashish@gmail.com>
2025-02-17 13:11:16 -0600

    changed connection from MySQL database to local

--- dbhelper.py
+++ dbhelper.py
@@ -1,154 +1,145 @@
-import mysql.connector
-import os
+import sqlite3
 import pandas as pd
 
 class DB:
     def __init__(self):
         try:
-            self.conn = mysql.connector.connect(
-                host='127.0.0.1',
-                user='root',
-                password='adhya',
-                database='revision'
-            )
+            # Load the local dataset (CSV file)
+            self.ipl = pd.read_csv("ipl.csv")
+
+            # Create an in-memory SQLite database
+            self.conn = sqlite3.connect(":memory:")
             self.mycursor = self.conn.cursor()
-            print('Connection established')
+
+            # Load the dataset into the SQLite database
+            self.ipl.to_sql("ipl", self.conn, index=False, if_exists="replace")
+            print('Local dataset loaded into SQLite in-memory database')
         except Exception as e:
-            print(f'Connection error: {e}')
-
-
+            print(f'Error: {e}')
 
     def fetch_player_name(self):
-
-        players = []
-        self.mycursor.execute("""
-                    SELECT DISTINCT batter 
-                    FROM ipl
-                    UNION
-                    SELECT DISTINCT bowler
-                    FROM ipl
-                    ORDER BY batter
-                    """)
-
-        data = self.mycursor.fetchall()
-
-        for items in data:
-            players.append(items[0])
-        return players
+        """Fetch all unique players (batters and bowlers) from the dataset."""
+        query = """
+            SELECT DISTINCT batter AS player 
+            FROM ipl
+            UNION
+            SELECT DISTINCT bowler
+            FROM ipl
+            ORDER BY player
+        """
+        self.mycursor.execute(query)
+        return [row[0] for row in self.mycursor.fetchall()]
 
     def fetch_bowler_name(self):
-
-        bowlers = []
-        self.mycursor.execute("""
-                    SELECT DISTINCT batter 
-                    FROM ipl
-                    UNION
-                    SELECT DISTINCT bowler
-                    FROM ipl
-                    ORDER BY batter
-                    """)
-
-        data = self.mycursor.fetchall()
-
-        for items in data:
-            bowlers.append(items[0])
-        return bowlers
-
-
-    def batsman_details(self,players):
-        self.mycursor.execute("""
-             SELECT bowling_team AS 'Against', SUM(batsman_runs) AS 'Run_scored'
-             ,ROUND(SUM(batsman_runs) / COUNT(DISTINCT id),2)  AS 'Average'
-            ,COUNT(DISTINCT id) AS 'Match_played',COUNT(ball) AS 'Ball_played'
-            , ROUND(SUM(batsman_runs) * 100 / COUNT(ball),2) as 'Sr'
-            ,COUNT(4s) AS 'Fours',COUNT(6s) AS 'Sixs'
+        """Fetch all unique bowlers from the dataset."""
+        query = """
+            SELECT DISTINCT bowler 
             FROM ipl
-            WHERE batter = '{}'
-            GROUP BY bowling_team  
-            """.format(players))
-        data = self.mycursor.fetchall()
-        return data
+            ORDER BY bowler
+        """
+        self.mycursor.execute(query)
+        return [row[0] for row in self.mycursor.fetchall()]
 
-    def bowler_details(self,bowlers):
-        self.mycursor.execute("""
+    def batsman_details(self, player):
+        """Fetch batting details for a specific player."""
+        query = """
+            SELECT 
+                bowling_team AS 'Against', 
+                SUM(batsman_runs) AS 'Run_scored',
+                ROUND(SUM(batsman_runs) * 1.0 / COUNT(DISTINCT id), 2) AS 'Average',
+                COUNT(DISTINCT id) AS 'Match_played',
+                COUNT(ball) AS 'Ball_played',
+                ROUND(SUM(batsman_runs) * 100.0 / COUNT(ball), 2) AS 'Sr',
+                SUM(CASE WHEN batsman_runs = 4 THEN 1 ELSE 0 END) AS 'Fours',
+                SUM(CASE WHEN batsman_runs = 6 THEN 1 ELSE 0 END) AS 'Sixs'
+            FROM ipl
+            WHERE batter = ?
+            GROUP BY bowling_team
+        """
+        self.mycursor.execute(query, (player,))
+        return self.mycursor.fetchall()
+
+    def bowler_details(self, bowler):
+        """Fetch bowling details for a specific bowler."""
+        query = """
             SELECT 
                 batting_team AS 'Opponent',
-                COUNT(DISTINCT id) AS 'Match Played',
-                SUM(wickets_per_match) AS 'Total Wicket',
-                SUM(overs_per_match) AS 'Total Overs',  
-                ROUND(SUM(maiden_balls) / 6) AS 'Maiden', 
-                SUM(total_runs_per_match) AS 'Runs Conceded',
-                ROUND(SUM(total_runs_per_match)/SUM(overs_per_match),2) AS 'Economy'
+                COUNT(DISTINCT id) AS 'Match_Played',
+                SUM(wickets_per_match) AS 'Total_Wicket',
+                SUM(overs_per_match) AS 'Total_Overs',
+                ROUND(SUM(maiden_balls) / 6.0) AS 'Maiden',
+                SUM(total_runs_per_match) AS 'Runs_Conceded',
+                ROUND(SUM(total_runs_per_match) * 1.0 / SUM(overs_per_match), 2) AS 'Economy'
             FROM (
                 SELECT 
                     batting_team,
                     id,
                     COUNT(bowler_wicket) AS wickets_per_match,
-                    COUNT(DISTINCT `over`) AS overs_per_match,  -- Overs per match
-                    COUNT(maiden) AS maiden_balls,  -- Total balls in maiden overs
+                    COUNT(DISTINCT `over`) AS overs_per_match,
+                    SUM(maiden) AS maiden_balls,
                     SUM(total_runs) AS total_runs_per_match
                 FROM ipl
-                WHERE bowler = '{}'
+                WHERE bowler = ?
                 GROUP BY batting_team, id
             ) AS match_stats
             GROUP BY batting_team
-                    """.format(bowlers))
-        data = self.mycursor.fetchall()
-        return data
+        """
+        self.mycursor.execute(query, (bowler,))
+        return self.mycursor.fetchall()
 
     def fetch_team(self):
-        teams = []
-        self.mycursor.execute("""
-        SELECT DISTINCT batting_team FROM ipl
-        """)
-
-        data = self.mycursor.fetchall()
-        for items in data:
-            teams.append(items[0])
-        return teams
+        """Fetch all unique teams from the dataset."""
+        query = """
+            SELECT DISTINCT batting_team 
+            FROM ipl
+        """
+        self.mycursor.execute(query)
+        return [row[0] for row in self.mycursor.fetchall()]
 
     def fetch_toss_decision(self):
-        toss = []
-
-        self.mycursor.execute("""
-        SELECT DISTINCT toss_decision FROM ipl
-        """)
-
-        data = self.mycursor.fetchall()
-
-        for items in data:
-            toss.append(items[0])
-        return toss
+        """Fetch all unique toss decisions from the dataset."""
+        query = """
+            SELECT DISTINCT toss_decision 
+            FROM ipl
+        """
+        self.mycursor.execute(query)
+        return [row[0] for row in self.mycursor.fetchall()]
 
     def head_to_head_compare(self, toss_decision, batting_team, bowling_team, toss_winner):
+        """Compare head-to-head stats based on toss decision, teams, and toss winner."""
         query = """
-            SELECT toss_winner AS 'Toss Winner',
-                   toss_decision AS 'Toss Decision',
-                   winner AS 'Winner',
-                   COUNT(DISTINCT id) AS 'Total Wins',
-                   SUM(COUNT(DISTINCT id)) OVER(PARTITION BY toss_winner) AS 'Total Game'
+            SELECT 
+                toss_winner AS 'Toss Winner',
+                toss_decision AS 'Toss Decision',
+                winner AS 'Winner',
+                COUNT(DISTINCT id) AS 'Total Wins',
+                SUM(COUNT(DISTINCT id)) OVER(PARTITION BY toss_winner) AS 'Total Game'
             FROM ipl
-            WHERE toss_decision = '{}' AND batting_team = '{}' AND bowling_team = '{}' AND toss_winner = '{}'
-            GROUP BY batting_team, bowling_team, winner, toss_decision, toss_winner
-        """.format(toss_decision, batting_team, bowling_team, toss_winner)
-
-        self.mycursor.execute(query)
-        data = self.mycursor.fetchall()
-        return data
-
+            WHERE toss_decision = ? 
+                AND batting_team = ? 
+                AND bowling_team = ? 
+                AND toss_winner = ?
+            GROUP BY toss_winner, toss_decision, winner
+        """
+        params = (toss_decision, batting_team, bowling_team, toss_winner)
+        self.mycursor.execute(query, params)
+        return self.mycursor.fetchall()
 
     def team_against_all(self, bowling_team, winner):
+        """Fetch team performance against all opponents."""
         query = """
-            SELECT t.winner AS 'Winner', t.oponent AS 'Opponent', COUNT(*) AS 'Total Win'
+            SELECT 
+                t.winner AS 'Winner', 
+                t.oponent AS 'Opponent', 
+                COUNT(*) AS 'Total Win'
             FROM (
                 SELECT DISTINCT id, bowling_team, batting_team AS oponent, winner
                 FROM ipl
-                WHERE bowling_team = %s AND winner = %s
+                WHERE bowling_team = ? AND winner = ?
                 ORDER BY bowling_team, winner, batting_team
             ) t
             GROUP BY t.bowling_team, t.oponent, t.winner
         """
         params = (bowling_team, winner)
         self.mycursor.execute(query, params)
-        data = self.mycursor.fetchall()
-        return data
+        return self.mycursor.fetchall()
